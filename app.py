f476833843c6bc2f7fc4d764c146a21c2c3cd68a
crypto-trading-master <trading_master@outlook.com>
2021-08-04 21:51:37 +0200

    FTX working

--- app.py
+++ app.py
@@ -1,22 +1,45 @@
 import json
-from flask import Flask, request, jsonify
+import ftx  # type: ignore
+import os
+from flask import Flask, request
+from pprint import pprint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+api_secret = os.getenv("API_SECRET")
+subaccount_name = os.getenv("SUBACCOUNT_NAME")
+
+client = ftx.FtxClient(api_key=api_key, api_secret=api_secret, subaccount_name=subaccount_name)
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@ app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 
-@app.route('/webhook', methods=['POST'])
+@ app.route('/webhook', methods=['POST'])
 def webhook():
 
     data = json.loads(request.data)
 
-    print(data["ticker"])
-    print(data["bar"])
+    action = data["action"]
+    print('Action:', action)
+
+    result = client.get_market('BTC-PERP')
+
+    if action == 'buy':
+        price = result['ask']
+    else:
+        price = result['bid']
+
+    result = client.get_account_info()
 
     return {
         "code": "succcess",
-        "message": data
+        "price": price,
+        "account info": result
     }
