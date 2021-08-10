66504502098d48cd08ff9685627f2e4273ae507d
crypto-trading-master <trading_master@outlook.com>
2021-08-10 17:18:11 +0200

    Undo g

--- app.py
+++ app.py
@@ -8,258 +8,170 @@ from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-with app.app_context():
+load_dotenv()
 
-    load_dotenv()
+api_key = os.getenv("API_KEY")
+api_secret = os.getenv("API_SECRET")
+subaccount_name = os.getenv("SUBACCOUNT_NAME")
+private_key = os.getenv("PRIVATE_KEY")
 
-    api_key = os.getenv("API_KEY")
-    api_secret = os.getenv("API_SECRET")
-    subaccount_name = os.getenv("SUBACCOUNT_NAME")
-<<<<<<< HEAD
-    g.private_key = os.getenv("PRIVATE_KEY")
-=======
-    private_key = os.getenv("PRIVATE_KEY")
->>>>>>> 31f192f677b9b23d0bb0b018443974c8996c39dc
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
-    with open('config.json', 'r') as f:
-        config = json.load(f)
+startBalance = config['startBalance']
+currBalance = config['startBalance']
+risk = config['risk']
+fees = config['fees']
+leverage = config['leverage']
 
-    startBalance = config['startBalance']
-    g.currBalance = config['startBalance']
-    risk = config['risk']
-    fees = config['fees']
-    leverage = config['leverage']
+client = ftx.FtxClient(api_key=api_key, api_secret=api_secret, subaccount_name=subaccount_name)
 
-    client = ftx.FtxClient(api_key=api_key, api_secret=api_secret, subaccount_name=subaccount_name)
-
-    g.positions = []  # type: list
-    g.trades = []  # type: list
-    g.alerts = []  # type: list
-
-<<<<<<< HEAD
-    print(g.currBalance)
+positions = []  # type: list
+trades = []  # type: list
+alerts = []  # type: list
 
 
-    @ app.route('/webhook', methods=['POST'])
-    def webhook():
+@ app.route('/webhook', methods=['POST'])
+def webhook():
 
-        print(g.private_key)
+    print(private_key)
 
-        data = json.loads(request.data)
+    data = json.loads(request.data)
 
-        if data['key'] != private_key:
-            return {
-                "status": "Private key error. Not authorized !"
-            }
+    if data['key'] != private_key:
+        return {
+            "status": "Private key error. Not authorized !"
+        }
 
-        ticker = data["ticker"]
-        action = data["action"]
+    ticker = data["ticker"]
+    action = data["action"]
 
-        alert = {}
-        alert["datetime"] = datetime.datetime.now()
-        alert["ticker"] = ticker
-        alert["action"] = action
-        g.alerts.append(alert)
+    alert = {}
+    alert["datetime"] = datetime.datetime.now()
+    alert["ticker"] = ticker
+    alert["action"] = action
+    alerts.append(alert)
 
-        openPosition = True
+    openPosition = True
 
-        result = client.get_market(ticker)
+    result = client.get_market(ticker)
 
-        bid = result["bid"]
-        ask = result["ask"]
+    bid = result["bid"]
+    ask = result["ask"]
 
-        if positions:
-            position = positions[0]
+    if positions:
+        position = positions[0]
 
-            # Check position type
+        # Check position type
 
-            positionAction = position["action"]
+        positionAction = position["action"]
 
-            if positionAction != action:
-                # Close position
+        if positionAction != action:
+            # Close position
 
-                if positionAction == 'buy':  # Sell
-                    price = bid
-                else:  # positionAction == sell -> Buy
-                    price = ask
+            if positionAction == 'buy':  # Sell
+                price = bid
+            else:  # positionAction == sell -> Buy
+                price = ask
 
-                coinAmount = position["coinAmount"]
-                positionCost = position["positionCost"]
+            coinAmount = position["coinAmount"]
+            positionCost = position["positionCost"]
 
-                trade = {}
-                trade["ticker"] = ticker
-                trade["datetime"] = datetime.datetime.now()
-                trade["action"] = action
-                trade["leverage"] = leverage
-                trade["position"] = position
+            trade = {}
+            trade["ticker"] = ticker
+            trade["datetime"] = datetime.datetime.now()
+            trade["action"] = action
+            trade["leverage"] = leverage
+            trade["position"] = position
 
-                feesAmount = coinAmount * price * fees
-                closeReturn = coinAmount * price - feesAmount
-
-                if action == 'buy':
-                    profit = positionCost - closeReturn
-                else:
-                    profit = closeReturn - positionCost
-
-                previousBalance = g.currBalance
-                g.currBalance += profit
-
-                profitPercent = (g.currBalance / previousBalance - 1) * leverage * 100
-
-                g.positions.pop(0)
-
-                trade["price"] = price
-                trade["previousBalance"] = previousBalance
-                trade["feesAmount"] = feesAmount
-                trade["closeReturn"] = closeReturn
-                trade["profit"] = profit
-                trade["profitPercent"] = profitPercent
-                trade["currentBalance"] = g.currBalance
-
-                g.trades.append(trade)
-
-=======
-
-    @ app.route('/webhook', methods=['POST'])
-    def webhook():
-
-        data = json.loads(request.data)
-
-        if data['key'] != private_key:
-            return {
-                "status": "Private key error. Not authorized !"
-            }
-
-        ticker = data["ticker"]
-        action = data["action"]
-
-        alert = {}
-        alert["datetime"] = datetime.datetime.now()
-        alert["ticker"] = ticker
-        alert["action"] = action
-        g.alerts.append(alert)
-
-        openPosition = True
-
-        result = client.get_market(ticker)
-
-        bid = result["bid"]
-        ask = result["ask"]
-
-        if positions:
-            position = positions[0]
-
-            # Check position type
-
-            positionAction = position["action"]
-
-            if positionAction != action:
-                # Close position
-
-                if positionAction == 'buy':  # Sell
-                    price = bid
-                else:  # positionAction == sell -> Buy
-                    price = ask
-
-                coinAmount = position["coinAmount"]
-                positionCost = position["positionCost"]
-
-                trade = {}
-                trade["ticker"] = ticker
-                trade["datetime"] = datetime.datetime.now()
-                trade["action"] = action
-                trade["leverage"] = leverage
-                trade["position"] = position
-
-                feesAmount = coinAmount * price * fees
-                closeReturn = coinAmount * price - feesAmount
-
-                if action == 'buy':
-                    profit = positionCost - closeReturn
-                else:
-                    profit = closeReturn - positionCost
-
-                previousBalance = g.currBalance
-                g.currBalance += profit
-
-                profitPercent = (g.currBalance / previousBalance - 1) * leverage * 100
-
-                g.positions.pop(0)
-
-                trade["price"] = price
-                trade["previousBalance"] = previousBalance
-                trade["feesAmount"] = feesAmount
-                trade["closeReturn"] = closeReturn
-                trade["profit"] = profit
-                trade["profitPercent"] = profitPercent
-                trade["currentBalance"] = g.currBalance
-
-                g.trades.append(trade)
-
->>>>>>> 31f192f677b9b23d0bb0b018443974c8996c39dc
-            else:
-                openPosition = False
-
-        # Open position
-
-        if openPosition:
+            feesAmount = coinAmount * price * fees
+            closeReturn = coinAmount * price - feesAmount
 
             if action == 'buy':
-                price = ask
+                profit = positionCost - closeReturn
             else:
-                price = bid
+                profit = closeReturn - positionCost
 
-            buyBalance = g.currBalance * risk
-            feesAmount = buyBalance * leverage * fees
-            coinAmount = (buyBalance * leverage - feesAmount) / price
-            positionCost = buyBalance * leverage
+            previousBalance = currBalance
+            currBalance += profit
 
-            position = {}
-            position["ticker"] = ticker
-            position["action"] = action
-            position["buyBalance"] = buyBalance
-            position["feesAmount"] = feesAmount
-            position["coinAmount"] = coinAmount
-            position["price"] = price
-            position["positionCost"] = positionCost
-            g.positions.append(position)
+            profitPercent = (currBalance / previousBalance - 1) * leverage * 100
 
-        return {
-            "code": "success"
-        }
+            positions.pop(0)
+
+            trade["price"] = price
+            trade["previousBalance"] = previousBalance
+            trade["feesAmount"] = feesAmount
+            trade["closeReturn"] = closeReturn
+            trade["profit"] = profit
+            trade["profitPercent"] = profitPercent
+            trade["currentBalance"] = currBalance
+
+            trades.append(trade)
+
+        else:
+            openPosition = False
+
+    # Open position
+
+    if openPosition:
+
+        if action == 'buy':
+            price = ask
+        else:
+            price = bid
+
+        buyBalance = currBalance * risk
+        feesAmount = buyBalance * leverage * fees
+        coinAmount = (buyBalance * leverage - feesAmount) / price
+        positionCost = buyBalance * leverage
+
+        position = {}
+        position["ticker"] = ticker
+        position["action"] = action
+        position["buyBalance"] = buyBalance
+        position["feesAmount"] = feesAmount
+        position["coinAmount"] = coinAmount
+        position["price"] = price
+        position["positionCost"] = positionCost
+        positions.append(position)
+
+    return {
+        "code": "success"
+    }
 
 
-    @ app.route('/status')
-    def status():
+@ app.route('/status')
+def status():
 
-        return {
-            "Start balance": startBalance,
-            "Current balance": g.currBalance,
-            "Number of trades": len(g.trades),
-            "Number of positions": len(g.positions),
-            "Number of alerts": len(g.alerts)
-        }
-
-
-    @ app.route('/trades')
-    def tradeStatus():
-
-        return {
-            "trades": g.trades
-        }
+    return {
+        "Start balance": startBalance,
+        "Current balance": currBalance,
+        "Number of trades": len(trades),
+        "Number of positions": len(positions),
+        "Number of alerts": len(alerts)
+    }
 
 
-    @ app.route('/positions')
-    def postionsStatus():
+@ app.route('/trades')
+def tradeStatus():
 
-        return {
-            "positions": g.positions
-        }
+    return {
+        "trades": trades
+    }
 
 
-    @ app.route('/alerts')
-    def alertsStatus():
+@ app.route('/positions')
+def postionsStatus():
 
-        return {
-            "alerts": g.alerts
-        }
+    return {
+        "positions": positions
+    }
+
+
+@ app.route('/alerts')
+def alertsStatus():
+
+    return {
+        "alerts": alerts
+    }
